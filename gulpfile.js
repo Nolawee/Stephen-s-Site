e3893842d436da1e46baf49c9211de4380669830
Nolawee Mengist <nome9478@colorado.edu>
2016-04-08 15:37:40 -0600

    More show dates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-ruby-sass'),
+	livereload = require('gulp-livereload'),
+	plumber = require('gulp-plumber'),
+	prefix = require('gulp-autoprefixer');;
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant'); // $ npm i -D imagemin-pngquant;
+
+function errorlog(error){
+	console.error.bind(error);
+	this.emit('end')
+}
+
 
 // Scripts Task
 // Uglifies
@@ -9,17 +21,37 @@ gulp.task('compress', function() {
     .pipe(gulp.dest('assets/js/minjs'));
 });
 
-// Scripts Task
+// Styles Task
 // Uglifies
 gulp.task('styles', function() {
-	console.log("this will make sense")
+	return sass('assets/scss/**/*.scss', {
+		style: 'expanded'
+	})
+		.on('error', errorlog)
+		.pipe(prefix({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('assets/css/uglify'))
+		.pipe(livereload());
+});
+
+// Image Task
+// Compress
+gulp.task('image',function(){
+	gulp.src('assets/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('assets/images/imgCompress'));
 });
 
 // Watch Task
 // Watches js
 gulp.task('watch', function() {
-	gulp.watch('assets/js/*.js',['compress']);
 
+	livereload.listen();
+
+	gulp.watch('assets/js/*.js',['compress']);
+	gulp.watch('assets/scss/**/*.scss',['styles'])
 });
 
-gulp.task('default', ['compress','styles']);
+gulp.task('default', ['compress','styles', 'image', 'watch']);
